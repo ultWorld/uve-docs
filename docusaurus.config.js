d8a55ac8ae85ae5e906e24a32d51a1dce3db93fd
Vivasvan Patel <vivasvan.patel@k-lens.de>
2023-03-05 06:24:55 +0530

    removing extra content from requirements.md

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/logoDark.png",
+      image: "img/logo.png",
       navbar: {
         title: "Documentation",
         logo: {
